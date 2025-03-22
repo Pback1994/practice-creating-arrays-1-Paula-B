b914fa95dbda522128b9deec7e7b9fb1b6fb832b
Paula Back <132527524+Pback1994@users.noreply.github.com>
2025-03-22 00:22:46 -0500

    Submitting Building Arrays - Lesson 1: Practice Creating Arrays assignment.

---
+++ practice-arrays.js
@@ -0,0 +1,15 @@
+//Task #1
+
+let groceryItems = ["bread", "milk", "cheese", "butter", "oil"];
+let prices = [12.99, 15.05, 14.51];
+let groceryItems2 = ["flour", "Sugar", "Oatmeal", "Apples", "Oranges"];
+
+//Task #2
+
+console.log(groceryItems);
+console.log(prices);
+console.log(groceryItems[0]);
+console.log(groceryItems[1]);
+console.log(groceryItems[2]);
+console.log(prices[2]);
+console.log(prices[0]);
